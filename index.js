4eb948489aa34f37c2e9c391820780ffe26ff907
Simon Damminga <99039973@mydavinci.nl>
2016-11-07 13:48:22 +0100

    mijn nieuwe website

---
+++ index.js
@@ -0,0 +1,36 @@
+function onMouseEnter(){
+	document.getElementById('naam').innerHTML = 'Mijn GitHub';
+}
+function onMouseLeave(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter1(){
+	document.getElementById('naam').innerHTML = 'Over mij';
+}
+function onMouseLeave1(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter2(){
+	document.getElementById('naam').innerHTML = 'Mijn bestanden';
+}
+function onMouseLeave2(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter3(){
+	document.getElementById('naam').innerHTML = 'Werk';
+}
+function onMouseLeave3(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter4(){
+	document.getElementById('naam').innerHTML = 'Nieuws';
+}
+function onMouseLeave4(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter5(){
+	document.getElementById('naam').innerHTML = 'Mijn studie';
+}
+function onMouseLeave5(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
