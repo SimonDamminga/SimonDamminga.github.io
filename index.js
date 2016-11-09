48b97132c6b588ff8541d31107579e1dcef4b0ef
Simon Damminga <99039973@mydavinci.nl>
2016-11-09 09:39:39 +0100

    nieuw

---
+++ index.js
@@ -0,0 +1,36 @@
+function onMouseEnter(){
+	document.getElementById('bestanden').innerHTML = 'Files';
+}
+function onMouseLeave(){
+	document.getElementById('bestanden').innerHTML = '<i class="fa fa-folder-open-o" aria-hidden="true"></i>';
+}
+function onMouseEnter1(){
+	document.getElementById('naam').innerHTML = 'Over mij';
+}
+function onMouseLeave1(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter2(){
+	document.getElementById('naam').innerHTML = 'Mijn bestanden';
+}
+function onMouseLeave2(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter3(){
+	document.getElementById('naam').innerHTML = 'Werk';
+}
+function onMouseLeave3(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter4(){
+	document.getElementById('naam').innerHTML = 'Nieuws';
+}
+function onMouseLeave4(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
+function onMouseEnter5(){
+	document.getElementById('naam').innerHTML = 'Mijn studie';
+}
+function onMouseLeave5(){
+	document.getElementById('naam').innerHTML = 'Simon Damminga';
+}
