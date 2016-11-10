81574e299312f611deb7efd9ecfc086818952749
Simon Damminga <99039973@mydavinci.nl>
2016-11-10 11:40:05 +0100

    update

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 function onMouseEnter(){
-	document.getElementById('bestanden').innerHTML = 'Files';
+	document.getElementById('bestanden').innerHTML = 'Bestanden';
+	document.getElementById('bestanden').style.cssText = 'font-size: 50px; font-weight: bold;'
 }
 function onMouseLeave(){
 	document.getElementById('bestanden').innerHTML = '<i class="fa fa-folder-open-o" aria-hidden="true"></i>';
+	document.getElementById('bestanden').style.cssText = 'font-size: 100px;';
 }
 function onMouseEnter1(){
-	document.getElementById('naam').innerHTML = 'Over mij';
+	document.getElementById('overmij').innerHTML = 'Over mij';
+	document.getElementById('overmij').style.cssText = 'font-size: 50px; font-weight: bold;';
 }
 function onMouseLeave1(){
-	document.getElementById('naam').innerHTML = 'Simon Damminga';
+	document.getElementById('overmij').innerHTML = '<i class="fa fa-user-circle-o" aria-hidden="true">';
+	document.getElementById('overmij').style.cssText = 'font-size: 100px;';
 }
 function onMouseEnter2(){
-	document.getElementById('naam').innerHTML = 'Mijn bestanden';
+	document.getElementById('school').innerHTML = 'Opleiding';
+	document.getElementById('school').style.cssText = 'font-size: 50px; font-weight: bold;';
 }
 function onMouseLeave2(){
-	document.getElementById('naam').innerHTML = 'Simon Damminga';
+	document.getElementById('school').innerHTML = '<i class="fa fa-graduation-cap" aria-hidden="true">';
+	document.getElementById('school').style.cssText = 'font-size: 100px;';
 }
 function onMouseEnter3(){
-	document.getElementById('naam').innerHTML = 'Werk';
+	document.getElementById('werk').innerHTML = 'Werk';
+	document.getElementById('werk').style.cssText = 'font-size: 50px; font-weight: bold;';
 }
 function onMouseLeave3(){
-	document.getElementById('naam').innerHTML = 'Simon Damminga';
+	document.getElementById('werk').innerHTML = '<i class="fa fa-briefcase" aria-hidden="true">';
+	document.getElementById('werk').style.cssText = 'font-size: 100px;';
 }
 function onMouseEnter4(){
-	document.getElementById('naam').innerHTML = 'Nieuws';
+	document.getElementById('contact').innerHTML = 'Contact';
+	document.getElementById('contact').style.cssText = 'font-size: 50px; font-weight: bold;';
 }
 function onMouseLeave4(){
-	document.getElementById('naam').innerHTML = 'Simon Damminga';
+	document.getElementById('contact').innerHTML = '<i class="fa fa-phone" aria-hidden="true">';
+	document.getElementById('contact').style.cssText = 'font-size: 100px;';
 }
 function onMouseEnter5(){
-	document.getElementById('naam').innerHTML = 'Mijn studie';
+	document.getElementById('github').innerHTML = 'Github';
+	document.getElementById('github').style.cssText = 'font-size: 50px; font-weight: bold;';
 }
 function onMouseLeave5(){
-	document.getElementById('naam').innerHTML = 'Simon Damminga';
+	document.getElementById('github').innerHTML = '<i class="fa fa-github" aria-hidden="true">';
+	document.getElementById('github').style.cssText = 'font-size: 100px;';
 }
