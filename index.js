9d8856f6f9733ce70905d7a0ff8a3e63c21ad9d0
Simon Damminga <99039973@mydavinci.nl>
2016-12-03 14:40:46 +0100

    jaa

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var height = false;
 function onMouseEnter(){
 	document.getElementById('bestanden').innerHTML = 'Bestanden';
 	document.getElementById('bestanden').style.cssText = 'font-size: 50px; font-weight: bold;'
@@ -45,4 +46,63 @@ function onMouseEnter5(){
 function onMouseLeave5(){
 	document.getElementById('github').innerHTML = '<i class="fa fa-github" aria-hidden="true">';
 	document.getElementById('github').style.cssText = 'font-size: 100px;';
+}
+function onMouseEnter6(){
+	document.getElementById('game').innerHTML = 'Opdracht Game';
+	document.getElementById('game').style.cssText = 'font-size: 50px; font-weight: bold;'
+}
+function onMouseLeave6(){
+	document.getElementById('game').innerHTML = '<i class="fa fa-gamepad" aria-hidden="true"></i>';
+	document.getElementById('game').style.cssText = 'font-size: 100px;';
+}
+function onMouseEnter7(){
+	document.getElementById('vloot').innerHTML = 'Opdracht Vloot';
+	document.getElementById('vloot').style.cssText = 'font-size: 50px; font-weight: bold;';
+}
+function onMouseLeave7(){
+	document.getElementById('vloot').innerHTML = '<i class="fa fa-ship" aria-hidden="true">';
+	document.getElementById('vloot').style.cssText = 'font-size: 100px;';
+}
+function onMouseEnter8(){
+	document.getElementById('space').innerHTML = 'Opdracht Space';
+	document.getElementById('space').style.cssText = 'font-size: 50px; font-weight: bold;';
+}
+function onMouseLeave8(){
+	document.getElementById('space').innerHTML = '<i class="fa fa-space-shuttle" aria-hidden="true">';
+	document.getElementById('space').style.cssText = 'font-size: 100px;';
+}
+function onMouseEnter9(){
+	document.getElementById('tools').innerHTML = 'Opdracht Handy';
+	document.getElementById('tools').style.cssText = 'font-size: 50px; font-weight: bold;';
+}
+function onMouseLeave9(){
+	document.getElementById('tools').innerHTML = '<i class="fa fa-gavel" aria-hidden="true">';
+	document.getElementById('tools').style.cssText = 'font-size: 100px;';
+}
+function onMouseEnter10(){
+	document.getElementById('layout').innerHTML = 'Opdracht Layout';
+	document.getElementById('layout').style.cssText = 'font-size: 50px; font-weight: bold;';
+}
+function onMouseLeave10(){
+	document.getElementById('layout').innerHTML = '<i class="fa fa-car" aria-hidden="true">';
+	document.getElementById('layout').style.cssText = 'font-size: 100px;';
+}
+function onMouseEnter11(){
+	document.getElementById('robot').innerHTML = 'Opdracht Robotarm (LUA)';
+	document.getElementById('robot').style.cssText = 'font-size: 50px; font-weight: bold;';
+}
+function onMouseLeave11(){
+	document.getElementById('robot').innerHTML = '<i class="fa fa-android" aria-hidden="true">';
+	document.getElementById('robot').style.cssText = 'font-size: 100px;';
+}
+function onMouseEnter12(){
+	document.getElementById('food').innerHTML = 'Opdracht Recepten';
+	document.getElementById('food').style.cssText = 'font-size: 50px; font-weight: bold;';
+}
+function onMouseLeave12(){
+	document.getElementById('food').innerHTML = '<i class="fa fa-cutlery" aria-hidden="true">';
+	document.getElementById('food').style.cssText = 'font-size: 100px;';
+}
+function doei(){
+	
 }
